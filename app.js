cc1865d067ade38309646ec1d47c7cb77409ed5b
richiProgrammer <richardadrian.ramirez@unet.edu.ve>
2022-11-13 19:40:20 -0400

    solution with jQuery window .on (load)

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
 $(document).ready(function() {
-    $( "#test" ).load( "http://www.tachira.gob.ve/wp-content/uploads/2014/09/Dra.-Raquel-Ariza.jpg", function() {
-        $(".placeholder").fadeOut();
-        
-      });
-})
+  $(window).on("load", function () {
+    $(".layer").fadeOut();
+    $(".show").fadeIn();
+  })
+}); 
 
-/*
-let test = document.getElementById("test");
-let array = Array.from(document.getElementsByClassName("placeholder"));
-
-if (test.complete) {
-    setTimeout(() => {
-        array.forEach(x => {
-            x.remove();
-        });
-    }, 2000);
-
-}*/
