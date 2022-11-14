120356cf6b914550ffabd95991e961c661b90a6f
richiProgrammer <richardadrian.ramirez@unet.edu.ve>
2022-11-14 17:36:08 -0400

    checking logo reload

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
   $(window).on("load", function () {
     $(".layer").fadeOut();
     $(".show").fadeIn();
-  })
+  });/*
+  $(".navbar-brand").click(function() {
+    location.reload();
+  })*/
 }); 
+
+let logo = document.querySelector(".navbar-brand");
+
+logo.onclick = () => {
+  location.reload();
+}
+
 
