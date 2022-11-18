e67ac0243d66b269b02e3887b4e4860003c7fb3e
richiProgrammer <richardadrian.ramirez@unet.edu.ve>
2022-11-18 19:54:58 -0400

    testing id load jquery, not working ok, for showing up an image without down printing

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 $(document).ready(function() {
+  $("#portada").on("load", function() {
+    $(this).removeClass("portada");
+  });
+
   $("#about-me").on("load", function() {
     $(".layer-sobremi").fadeOut();
-    console.log("sobre mi OK")
-  })
-  $(".picture1").on("load", function() {
+  });
+
+  $(window).on("load", function() {
     $(".layer").fadeOut();
     $(".showup").fadeIn();
-    console.log("loaded")
   });
+
   $(".navbar-brand").click(function() {
     location.reload();
   });
+
   $("#ver-mas").click(function() {
     $("#ver-mas-loader").toggleClass("mostrar");
     $("#ver-mas").text($(this).text() == "Ver más entrevistas" ? "Ver menos entrevistas" : "Ver más entrevistas");
     setTimeout(() => {
       $("#ver-mas-loader").addClass("mostrar");
     }, 500);
-  })
+  });
 }); 
-
-new PerformanceObserver((entryList) => {
-  for (const entry of entryList.getEntries()) {
-    console.log('LCP candidate:', entry.startTime, entry);
-  }
-}).observe({type: 'largest-contentful-paint', buffered: true});
-
