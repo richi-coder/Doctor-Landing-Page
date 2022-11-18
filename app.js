ab5d54baa5ba550defaf55493213ade0ef711e16
richiProgrammer <richardadrian.ramirez@unet.edu.ve>
2022-11-17 23:35:59 -0400

    updating sobre mi position, first showup of the page, button show more

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ $(document).ready(function() {
   });
   $("#ver-mas").click(function() {
     $("#ver-mas-loader").toggleClass("mostrar");
+    $("#ver-mas").text($(this).text() == "Ver más entrevistas" ? "Ver menos entrevistas" : "Ver más entrevistas");
+    setTimeout(() => {
+      $("#ver-mas-loader").addClass("mostrar");
+    }, 500);
   })
 }); 
-/*
-let logo = document.querySelector(".navbar-brand");
 
-logo.onclick = () => {
-  location.reload();
-}
-
-*/
+new PerformanceObserver((entryList) => {
+  for (const entry of entryList.getEntries()) {
+    console.log('LCP candidate:', entry.startTime, entry);
+  }
+}).observe({type: 'largest-contentful-paint', buffered: true});
